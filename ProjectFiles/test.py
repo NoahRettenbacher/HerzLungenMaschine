2cdffd134605c19a973f1b2664893c760f193a3f
NoahRettenbacher <you@example.com>
2022-05-16 11:14:06 +0200

    Aufgabe 2.1 ansatz

--- ProjectFiles/test.py
+++ ProjectFiles/test.py
@@ -18,12 +18,23 @@ subj_numbers = []
 number_of_subjects = 0
 
 folder_current = os.path.dirname(__file__) 
-print(folder_current) #= projekt files
+#print(folder_current) #= projekt files
 folder_input_data = os.path.join(folder_current, "input_data")
 for file in os.listdir(folder_input_data):
     
     if file.endswith(".csv"):
         number_of_subjects += 1
         file_name = os.path.join(folder_input_data, file)
-        print(file_name)
+        #print(file_name)
         list_of_subjects.append(ut.Subject(file_name))
+
+df = list_of_subjects[0].subject_data
+
+grp=df[['SpO2 (%)','Temp (C)', 'Blood Flow (ml/s)']].agg(['max','idxmax','min','idxmin'])
+#print(grp)
+y=grp.loc[['max','min']]
+print(y)
+x=grp.loc[['idxmax','idxmin']]
+print(x)
+
+
